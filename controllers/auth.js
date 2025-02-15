fe5ef90bddea230fb70f1751844eae6760d6cc85
NargessAlmahdi <nargess.almahdi2002@gmail.com>
2025-02-16 01:52:57 +0300

    Back-end completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,7 +48,7 @@ router.post('/sign-in', async (req, res) => {
 
     const token = jwt.sign({ payload }, process.env.JWT_SECRET);
 
-    res.status(200).json({ token });
+    res.status(201).json({ token });
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// npm
+// NPM
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -7,10 +7,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
 
-// Import routers
+// IMPORT ROUTERS
 const authRouter = require('./controllers/auth');
 const testJwtRouter = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
+const hootsRouter = require("./controllers/hoots.js");
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI);
@@ -24,10 +25,13 @@ app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
-// Routes
+// ROUTES
 app.use('/auth', authRouter);
 app.use('/test-jwt', testJwtRouter);
 app.use('/users', usersRouter);
+app.use("/hoots", hootsRouter);
+
+
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
